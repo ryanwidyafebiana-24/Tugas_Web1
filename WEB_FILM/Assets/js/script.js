c8b3bb35141c9bd8f373c4953e98020ea90da153
ryanwidyafebiana-24 <widyariyan550@gmail.com>
2025-08-14 07:34:57 +0700

    Add files via upload

---
+++ WEB_FILM/Assets/js/script.js
@@ -0,0 +1,7 @@
+function tampilkanVideo() {
+    document.getElementById('videoContainer').style.display = 'flex';
+}
+
+function tutupVideo() {
+    document.getElementById('videoContainer').style.display = 'none';
+}
